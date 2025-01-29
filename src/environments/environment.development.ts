e48b1d4edc2fa1d78069ed918fe233db29015c6c
anuja13 <anujagit@gmail.com>
2025-01-29 11:57:11 +0100

    endpoints incudes server address

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,11 +2,11 @@ export const environment = {
     production: true,
     backend: {
       endpoints:{
-        baseUrl: "http://localhost:8014/dashboard/getPatientInfo",
-        activateUrl: "http://localhost:8014/dashboard/activate",
-        RCTGroupUrl: "http://localhost:8014/dashboard/setRCTGroup",
-        deactivateUrl: "http://localhost:8014/dashboard/deactivate",
-        deleteUrl: "http://localhost:8014/dashboard/delete"
+        baseUrl: "http://smartwork.idi.ntnu.no:8014/dashboard/getPatientInfo",
+        activateUrl: "http://smartwork.idi.ntnu.no:8014/dashboard/activate",
+        RCTGroupUrl: "http://smartwork.idi.ntnu.no:8014/dashboard/setRCTGroup",
+        deactivateUrl: "http://smartwork.idi.ntnu.no:8014/dashboard/deactivate",
+        deleteUrl: "http://smartwork.idi.ntnu.no:8014/dashboard/delete"
        
       }
     },
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@ export const environment = {
     production: false,
     backend: {
       endpoints:{
-        baseUrl: "http://localhost:8014/dashboard/getPatientInfo",
-        activateUrl: "http://localhost:8014/dashboard/activate",
-        RCTGroupUrl: "http://localhost:8014/dashboard/setRCTGroup",
-        deactivateUrl: "http://localhost:8014/dashboard/deactivate",
-        deleteUrl: "http://localhost:8014/dashboard/delete"
+        baseUrl: "http://smartwork-dev.idi.ntnu.no:8014/dashboard/getPatientInfo",
+        activateUrl: "http://smartwork-dev.idi.ntnu.no:8014/dashboard/activate",
+        RCTGroupUrl: "http://smartwork-dev.idi.ntnu.no:8014/dashboard/setRCTGroup",
+        deactivateUrl: "http://smartwork-dev.idi.ntnu.no:8014/dashboard/deactivate",
+        deleteUrl: "http://smartwork-dev.idi.ntnu.no:8014/dashboard/delete"
       }
     },
     defaults:{
