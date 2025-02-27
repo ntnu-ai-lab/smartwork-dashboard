6f3928715b0c36b16275fe0e86979b6cffe03c71
stuartgo <ottersenstuart@gmail.com>
2025-02-27 08:22:02 +0100

    Fix urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@ export const environment = {
     production: true,
     backend: {
       endpoints:{
-        baseUrl: "http://smartwork-dev-backend-limesurvey.idi.ntnu.no/dashboard/getPatientInfo",
-        activateUrl: "http://smartwork-dev-backend-limesurvey.idi.ntnu.no/dashboard/activate",
-        RCTGroupUrl: "http://smartwork-dev-backend-limesurvey.idi.ntnu.no/dashboard/setRCTGroup",
-        deactivateUrl: "http://smartwork-dev-backend-limesurvey.idi.ntnu.no/dashboard/deactivate",
-        deleteUrl: "http://smartwork-dev-backend-limesurvey.idi.ntnu.no/dashboard/delete"
+        baseUrl: "https://smartwork-dev-backend-limesurvey.idi.ntnu.no/dashboard/getPatientInfo",
+        activateUrl: "https://smartwork-dev-backend-limesurvey.idi.ntnu.no/dashboard/activate",
+        RCTGroupUrl: "https://smartwork-dev-backend-limesurvey.idi.ntnu.no/dashboard/setRCTGroup",
+        deactivateUrl: "https://smartwork-dev-backend-limesurvey.idi.ntnu.no/dashboard/deactivate",
+        deleteUrl: "https://smartwork-dev-backend-limesurvey.idi.ntnu.no/dashboard/delete"
       }
     },
     defaults:{
