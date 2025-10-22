c6c47ac53df74e3c859459c0c08f44b547514a06
Christoph Eder <ce1@live.de>
2025-10-22 11:03:25 +0200

    Changes in config.service.ts     Dynamically load endpoints

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -4,13 +4,15 @@ import { Injectable } from '@angular/core';
 export class ConfigService {
   private env = (window as any).env || {};
 
-  readonly endpoints = {
-    baseUrl: this.env.BASE_URL || 'http://localhost:8014/dashboard/getPatientInfo',
-    activateUrl: this.env.ACTIVATE_URL || 'http://localhost:8014/dashboard/activate',
-    updatedConsentURL: this.env.UPDATED_CONSENT_URL || 'http://localhost:8014/dashboard/updatedConsent',
-    RCTGroupUrl: this.env.RCTGROUP_URL || 'http://localhost:8014/dashboard/setRCTGroup',
-    deactivateUrl: this.env.DEACTIVATE_URL || 'http://localhost:8014/dashboard/deactivate',
-    deleteUrl: this.env.DELETE_URL || 'http://localhost:8014/dashboard/delete',
-    resetPasswordUrl: this.env.RESET_PASSWORD_URL || 'http://localhost:8014/dashboard/resetPassword'
-  };
+  get endpoints() {
+    return {
+        baseUrl: this.env.BASE_URL || 'http://localhost:8014/dashboard/getPatientInfo',
+        activateUrl: this.env.ACTIVATE_URL || 'http://localhost:8014/dashboard/activate',
+        updatedConsentURL: this.env.UPDATED_CONSENT_URL || 'http://localhost:8014/dashboard/updatedConsent',
+        RCTGroupUrl: this.env.RCTGROUP_URL || 'http://localhost:8014/dashboard/setRCTGroup',
+        deactivateUrl: this.env.DEACTIVATE_URL || 'http://localhost:8014/dashboard/deactivate',
+        deleteUrl: this.env.DELETE_URL || 'http://localhost:8014/dashboard/delete',
+        resetPasswordUrl: this.env.RESET_PASSWORD_URL || 'http://localhost:8014/dashboard/resetPassword'
+    };
+  }
 }
