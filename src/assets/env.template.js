6ac1d8ce3b2db7e587796109512f3d5ba9e2e025
Christoph Eder <ce1@live.de>
2025-10-22 10:03:32 +0200

    Trying to consolidate to one .env file
    Added env.template.js
        File that at runtime loads the endpoints from the .env
        Is replaced at container creation with env.js
    
    Changes in environment*.ts
        Using the new env.js file
    
    Changes in index.html
        Added env.js script

---
+++ src/assets/env.template.js
@@ -0,0 +1,10 @@
+(function(window) {
+  window.env = window.env || {};
+  window.env.BASE_URL = "${BASE_URL}";
+  window.env.ACTIVATE_URL = "${ACTIVATE_URL}";
+  window.env.RCTGROUP_URL = "${RCTGROUP_URL}";
+  window.env.DEACTIVATE_URL = "${DEACTIVATE_URL}";
+  window.env.DELETE_URL = "${DELETE_URL}";
+  window.env.RESET_PASSWORD_URL = "${RESET_PASSWORD_URL}";
+  window.env.UPDATED_CONSENT_URL = "${UPDATED_CONSENT_URL}";
+})(this);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,13 +10,21 @@ export const environment = {
         // resetPasswordUrl: "https://smartwork-dashboard.idi.ntnu.no/dashboard/resetPassword",
         // updatedConsentURL: "https://smartwork-dashboard.idi.ntnu.no/dashboard/updatedConsent"
         
-        baseUrl: "http://localhost:8014/dashboard/getPatientInfo",
-        activateUrl: "http://localhost:8014/dashboard/activate",
-        RCTGroupUrl: "http://localhost:8014/dashboard/setRCTGroup",
-        deactivateUrl: "http://localhost:8014/dashboard/deactivate",
-        deleteUrl: "http://localhost:8014/dashboard/delete",
-        resetPasswordUrl: "http://localhost:8014/dashboard/resetPassword",
-        updatedConsentURL: "http://localhost:8014/dashboard/updatedConsent"
+        // baseUrl: "http://localhost:8014/dashboard/getPatientInfo",
+        // activateUrl: "http://localhost:8014/dashboard/activate",
+        // RCTGroupUrl: "http://localhost:8014/dashboard/setRCTGroup",
+        // deactivateUrl: "http://localhost:8014/dashboard/deactivate",
+        // deleteUrl: "http://localhost:8014/dashboard/delete",
+        // resetPasswordUrl: "http://localhost:8014/dashboard/resetPassword",
+        // updatedConsentURL: "http://localhost:8014/dashboard/updatedConsent"
+        baseUrl: (window as any).env?.BACKEND_BASE_URL || "http://localhost:8014/dashboard/getPatientInfo",
+        activateUrl: (window as any).env?.ACTIVATE_URL || "http://localhost:8014/dashboard/activate",
+        RCTGroupUrl: (window as any).env?.RCTGROUP_URL || "http://localhost:8014/dashboard/setRCTGroup",
+        deactivateUrl: (window as any).env?.DEACTIVATE_URL || "http://localhost:8014/dashboard/deactivate",
+        deleteUrl: (window as any).env?.DELETE_URL || "http://localhost:8014/dashboard/delete",
+        resetPasswordUrl: (window as any).env?.RESET_PASSWORD_URL || "http://localhost:8014/dashboard/resetPassword",
+        updatedConsentURL: (window as any).env?.UPDATED_CONSENT_URL || "http://localhost:8014/dashboard/updatedConsent"
+    
       }
     },
 };
